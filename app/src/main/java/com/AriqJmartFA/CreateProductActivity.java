80a62cb812c06e3cfc502bf80d871b6809672d0e
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-05 23:49:11 +0700

    creating json parsing on productfragment

--- app/src/main/java/com/AriqJmartFA/CreateProductActivity.java
+++ app/src/main/java/com/AriqJmartFA/CreateProductActivity.java
@@ -1,14 +1,193 @@
 package com.AriqJmartFA;
 
+import static com.AriqJmartFA.LoginActivity.loggedAccount;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CreateProductActivity extends AppCompatActivity {
+import com.AriqJmartFA.model.ProductCategory;
+import com.AriqJmartFA.request.CreateProductRequest;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class CreateProductActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String conditionUsed;
+    public String[] shipmentString = {"INSTANT", "SAME DAY", "NEXT DAY", "REGULER", "KARGO"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_product);
+
+        EditText productName = findViewById(R.id.product_name);
+        EditText productWeight = findViewById(R.id.product_weight);
+        EditText productPrice = findViewById(R.id.product_price);
+        EditText productDiscount = findViewById(R.id.product_discount);
+        RadioGroup radioCondition = findViewById(R.id.radio_condition);
+
+        Spinner categorySpinner = findViewById(R.id.category_spinner);
+        categorySpinner.setOnItemSelectedListener(this);
+        Spinner shipmentplanSpinner = findViewById(R.id.shipmentplan_spinner);
+        shipmentplanSpinner.setOnItemSelectedListener(this);
+
+        Button createButton = findViewById(R.id.create_button);
+        Button cancelButton = findViewById(R.id.cancel_button);
+
+        ArrayList<String> categoryList = new ArrayList<String>();
+        for(ProductCategory category : ProductCategory.values()) {
+
+            categoryList.add(category.toString());
+
+        }
+
+        ArrayAdapter productAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categoryList);
+        productAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(productAdapter);
+
+        ArrayAdapter shipmentAdapter =new ArrayAdapter(this, android.R.layout.simple_spinner_item, shipmentString);
+        shipmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        shipmentplanSpinner.setAdapter(shipmentAdapter);
+
+        radioCondition.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup radioCondition, int checkedId) {
+
+                int selectedID = radioCondition.getCheckedRadioButtonId();
+
+                if(selectedID == R.id.radio_new) {
+
+                    conditionUsed = "false";
+
+                } else if(selectedID == R.id.radio_used) {
+
+                    conditionUsed = "true";
+
+                }
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Response.Listener<String> listener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            if(object != null) {
+
+                                Toast.makeText(CreateProductActivity.this, "Product has been added!", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(CreateProductActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        } catch(JSONException e) {
+
+                            Toast.makeText(CreateProductActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                };
+
+                Response.ErrorListener errorListener = errorResponse -> {
+
+                };
+
+                String shipment = shipmentplanSpinner.getSelectedItem().toString();
+                String shipmentPlanByte;
+                switch(shipment) {
+                    case "INSTANT": {
+
+                        shipmentPlanByte = "1";
+                        break;
+
+                    }
+                    case "SAME DAY": {
+
+                        shipmentPlanByte = "2";
+                        break;
+
+                    }
+                    case "NEXT DAY": {
+
+                        shipmentPlanByte = "4";
+                        break;
+
+                    }
+                    case "REGULER": {
+
+                        shipmentPlanByte = "8";
+                        break;
+
+                    }
+                    case "KARGO": {
+
+                        shipmentPlanByte = "16";
+                        break;
+
+                    }
+                    default: {
+
+                        shipmentPlanByte = "1";
+
+                    }
+                }
+
+                CreateProductRequest createProduct = new CreateProductRequest(
+                        Integer.toString(loggedAccount.id),
+                        productName.getText().toString(),
+                        productWeight.getText().toString(),
+                        conditionUsed,
+                        productPrice.getText().toString(),
+                        productDiscount.getText().toString(),
+                        categorySpinner.getSelectedItem().toString(),
+                        shipmentPlanByte,
+                        listener,
+                        errorListener);
+
+                RequestQueue requestQueue = Volley.newRequestQueue(CreateProductActivity.this);
+                requestQueue.add(createProduct);
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CreateProductActivity.this, "Create product cancelled", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(CreateProductActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/AriqJmartFA/fragment/ProductFragment.java
+++ app/src/main/java/com/AriqJmartFA/fragment/ProductFragment.java
@@ -1,14 +1,36 @@
 package com.AriqJmartFA.fragment;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.AriqJmartFA.MainActivity;
 import com.AriqJmartFA.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,12 +77,123 @@ public class ProductFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        GetData getData = new GetData();
+        getData.execute();
+
+        /*
+
+        ListView lv = (ListView)getActivity().findViewById(R.id.product_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                productList);
+
+        lv.setAdapter(adapter);
+
+         */
     }
+
+
+    String accountId, name, weight, conditionUsed, price, discount, category, shipmentPlan;
+    private static String JSON_URL = "localhost:8080/product/getProductList";
+    ArrayList<HashMap<String,String>> productList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Context context = container.getContext();
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product, container, false);
+
+    }
+
+    public class GetData extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String current = "";
+
+            try {
+                URL url;
+                HttpURLConnection urlConnection = null;
+
+                try {
+                    url = new URL(JSON_URL);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(in);
+
+                    int data = isr.read();
+                    while(data != -1) {
+                        current += (char) data;
+                        data = isr.read();
+                    }
+
+                    return current;
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                } finally {
+
+                    if(urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+            return current;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            try{
+                JSONArray jsonArray = new JSONArray(s);
+
+                for(int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject object = jsonArray.getJSONObject(i);
+
+                    accountId = object.getString("accountId");
+                    name = object.getString("name");
+                    weight = object.getString("weight");
+                    conditionUsed = object.getString("conditionUsed");
+                    price = object.getString("price");
+                    discount = object.getString("discount");
+                    category = object.getString("category");
+                    shipmentPlan = object.getString("shipmentPlan");
+
+                    HashMap<String, String> products = new HashMap<String, String>();
+
+                    products.put("accountId", accountId);
+                    products.put("name", name);
+                    products.put("weight", weight);
+                    products.put("conditionUsed", conditionUsed);
+                    products.put("price", price);
+                    products.put("discount", discount);
+                    products.put("category", category);
+                    products.put("shipmentPlan", shipmentPlan);
+
+                    productList.add(products);
+
+
+                }
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+
+            }
+        }
     }
 }
